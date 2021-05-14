3052919f8c5a53e554cb6d7fcd56000c6ed254ba
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-05-14 13:34:18 -0400

    Implementada función bubblesort!()

--- main.jl
+++ main.jl
@@ -1,7 +1,20 @@
 function bubblesort!(arr::AbstractVector)
-
+    for _ = 2:length(arr), j = 1:length(arr)-1
+        if arr[j] > arr[j+1]
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+        end
+    end
+    return arr
 end
 
 function removeDuplicates(arr::AbstractVector)
 
 end
+
+v = [4, 1, -3, 6, 10, 4]
+
+println(v)
+
+bubblesort!(v)
+
+println(v)
